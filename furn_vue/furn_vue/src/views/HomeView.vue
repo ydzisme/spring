8c0ef72627e08f455fbf620f1e49efde7c84139b
ydz <3474923010@qq.com>
2025-08-06 18:01:20 +0800

    实现了页面展示数据库内容，并且添加之后及时刷新页面信息

--- furn_vue/furn_vue/src/views/HomeView.vue
+++ furn_vue/furn_vue/src/views/HomeView.vue
@@ -11,9 +11,12 @@
           <el-button style="margin-left: 10px" type="primary">搜索</el-button>
         </div>
         <el-table :data="tableData" stripe style="width: 100%">
-          <el-table-column sortable prop="date" label="日期"/>
+          <el-table-column sortable prop="id" label="ID"/>
           <el-table-column prop="name" label="商品名"/>
-          <el-table-column prop="address" label="生产商"/>
+          <el-table-column prop="maker" label="生产商"/>
+          <el-table-column prop="price" label="价格"/>
+          <el-table-column prop="sales" label="销量"/>
+          <el-table-column prop="stock" label="库存"/>
           <el-table-column fixed="right" label="操作" min-width="120">
             <template #default="scope">
               <el-button link type="primary" size="small" @click="handleEdit()">
@@ -63,29 +66,11 @@ export default {
       search: '',
       dialogVisible: false,
       form: {},
-      tableData: [
-        {
-          date: '2016-05-03',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-02',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-04',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-01',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-      ]
+      tableData: []
     }
+  },
+  created() {
+    this.findAll()
   },
   methods: {
     add() {
@@ -96,6 +81,13 @@ export default {
       request.post("api/ssm/save",this.form).then(res =>{
         console.log("res",res)
         this.dialogVisible = false
+        this.findAll()
+      })
+    },
+    findAll() {
+      request.get(("api/ssm/findAll")).then(res => {
+        console.log("res",res)
+        this.tableData = res.extend.furns
       })
     }
   }
